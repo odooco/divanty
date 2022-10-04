a89fcd1076659ea433983e33f7c222a1f8d1729f
coondev-michael-colorado <desarrollo@coondev.com>
2022-10-04 11:39:29 -0500

    Correccion de Coneccion de Shippiffuy

--- eg_shopify_integration_lite/models/product_template.py
+++ eg_shopify_integration_lite/models/product_template.py
@@ -15,6 +15,12 @@ _logger = logging.getLogger("=== Import Product Template ===")
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    def import_product_all(self):
+        for item in self:
+            instance_ids = self.env['eg.ecom.instance'].search([('provider', '=', 'eg_shopify')])
+            for instance_id in instance_ids:
+                item.import_product_from_shopify(instance_id)
+
     def import_product_from_shopify(self, instance_id=None, product_image=None, default_product_id=None):
         status = "no"
         text = ""
